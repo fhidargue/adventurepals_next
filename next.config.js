ff74e78274e6b4e25378eeeb75fd4b5c39303dbc
fhidalgo <fhidalgo@konradgroup.com>
2023-01-08 19:56:09 -0600

    added package json and next config properties

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    swcMinify: true,
+    images: {
+        unoptimized: true,
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
